9a66c0a7c31f13e7842d7160a0bd847de41f35d5
aaronumen219 <aaronumen219@gmail.com>
2020-09-16 15:53:33 +0000

    Finished part 1

--- main.cs
+++ main.cs
@@ -2,6 +2,28 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+  
+  Console.WriteLine ("Enter temperature in Celsius");
+  double temperature = Convert.ToDouble(Console.ReadLine());
+
+  double farenheit = Convert.ToDouble(((temperature * 9)/5) + 32);
+
+  Console.WriteLine("Farenheit equivalent for " + temperature + " degree celsius is " + farenheit);
+
+  if  (temperature < 0)
+    {
+  
+      Console.WriteLine("This temperature is below freezing of 32 degree Farenheit");
+
+    }
+    else if (temperature > 100)
+    {
+      Console.WriteLine("This temperature is above boiling point of 212 degree Farenheit");
+    }
+   
+
+
+
+
   }
 }
